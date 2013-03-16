3d3a5a89cefecc89738911fba95932ae0ca512e8
Victor Hugo <victornovais@Ubuntu.(none)>
2013-03-16 17:02:18 -0300

    Criando primeiros modelos com Hibernate

--- src/com/sixcodes/model/User.java
+++ src/com/sixcodes/model/User.java
@@ -1,5 +1,21 @@
 package com.sixcodes.model;
 
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String firstname;
+	
+	private String lastname;
+	
+	@Column(unique=True)
+	private String username;
+	
+	private String password;
+	
+	@Cloumn(unque=True)
+	private String email;
 }
