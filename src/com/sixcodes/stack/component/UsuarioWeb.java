ffeb1cb8040678d7597960270997d585c17c3829
Jesue Junior <jesuesousa@gmail.com>
2013-03-27 03:43:43 -0300

    Adicionando componet para logar na aplicação

---
+++ src/com/sixcodes/stack/component/UsuarioWeb.java
@@ -0,0 +1,29 @@
+package com.sixcodes.stack.component;
+import java.io.Serializable;
+
+import com.sixcodes.stack.model.Usuario;
+
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.SessionScoped;
+
+@Component
+@SessionScoped
+public class UsuarioWeb implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Usuario logado;
+	public void login(Usuario usuario){
+		this.logado = usuario;
+	}
+	public String getNome(){
+		return logado.getFirstname();
+	}
+	
+	public boolean isLogado(){
+		return logado != null;
+	}
+	public void logout(){
+		this.logado = null;
+	}
+}
